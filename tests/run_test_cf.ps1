104c489f89b3f8c88d473bedb4b33d0c3462d033
BrandonStudio <brandonstudio@outlook.com>
2026-05-14 16:44:45 +0800

    debug 7

--- tests/run_test_cf.ps1
+++ tests/run_test_cf.ps1
@@ -11,6 +11,9 @@ $tests = @(
 $url = "$env:CF_WORKER_URL/mcp"
 $commandArgs = "$url --transport http".Split(" ")
 if ($env:CF_ACCESS_CLIENT_ID -and $env:CF_ACCESS_CLIENT_SECRET) {
+    if ($env:CF_ACCESS_CLIENT_ID.StartsWith("`"")) {
+        Write-Host "WARNING: quoted"
+    }
     $commandArgs += "--header"
     $commandArgs += "CF-Access-Client-Id: $env:CF_ACCESS_CLIENT_ID"
     $commandArgs += "--header"
